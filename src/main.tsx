b1e31f171a4a2b0ce46c9f994a7099d4b77e2259
EJ <e@j>
2023-04-20 02:47:37 -0400

    Bare minimum app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {App} from './App';
 import './index.css';
+import {Grommet} from "grommet";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <Grommet>
+            <App />
+        </Grommet>
     </React.StrictMode>,
 );
